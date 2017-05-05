a226936516f9bff5a0f8fca3f00f17f72a2ff9b2
Raghu Betina <raghu@firstdraft.com>
2017-05-04 21:28:33 -0500

    wip

--- lib/generators/starter/resource/templates/factories.rb
+++ lib/generators/starter/resource/templates/factories.rb
@@ -1,12 +1,12 @@
 class IncreasingRandom
   attr_reader :value
 
-  def initialize(value = rand(100))
+  def initialize(value = rand(1..100))
     @value = value
   end
 
   def next
-    next_value = value + rand(100)
+    next_value = value + rand(1..100)
     IncreasingRandom.new(next_value)
   end
 
